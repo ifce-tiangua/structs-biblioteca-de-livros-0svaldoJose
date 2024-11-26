803a1350bfac701bff9068c1ab72adf7b588e270
Osvaldo <107486317+0svaldoJose@users.noreply.github.com>
2024-11-26 17:21:01 -0300

    05

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct livros {
-    char nome[50];
-    char isbn[20];
-    double pre;
-    int score;
-    char edi[40];
-} livro, *pLivro;
+typedef struct livros{
+	char nome[50];
+	char isbn[20];
+	double pre;
+	int score;
+	char edi[40];
+}livro, *pLivro;
 
-pLivro aloc(int qtde) {
-    pLivro livros = (pLivro)malloc(qtde * sizeof(livro));
-    return livros;
+pLivro aloc(int qtde){
+	
+	pLivro livros = (pLivro)malloc(qtde * sizeof(livro));
+	
+	return livros;
 }
 
-void preencher(pLivro livros, int qtde) {
-    int i;
-    for (i = 0; i < qtde; i++) {
-        scanf(" %[^\n]", livros[i].nome);
-        scanf(" %[^\n]", livros[i].isbn);
-        scanf("%lf", &livros[i].pre);
-        scanf("%d", &livros[i].score);
-        scanf(" %[^\n]", livros[i].edi); 
-    }
-}
+void preencher(pLivro livros, int qtde){
+	int i;
+	for(i = 0; i < qtde; i++){
+
+	scanf(" %[^\n]", livros[i].nome);
+	scanf("%s", livros[i].isbn);
+	scanf("%lf", &livros[i].pre);
+	scanf("%d", &livros[i].score);
+	scanf(" %[^\n]", livros[i].edi);
+	
+   }	
+ } 
 
 void mostrar(pLivro livros, int qtde) {
     int i;
@@ -33,31 +37,32 @@ void mostrar(pLivro livros, int qtde) {
         printf("ISBN: %s\n", livros[i].isbn);
         printf("Preço: R$ %.2lf\n", livros[i].pre);
         printf("Score: %d\n", livros[i].score);
-        printf("Edição: %s\n", livros[i].edi);
+        printf("Editora: %s\n", livros[i].edi);
         
         if (i < qtde - 1)
             printf("\n");
     }
 }
 
-void desaloc(pLivro livros) {
-    free(livros);
+
+void desaloc(pLivro livros){
+	free(livros);
 }
 
-int main() {
-    int qtde;
-    
-    scanf("%d", &qtde);
-    getchar();
-
-    if (qtde <= 0)
-        printf("Sem livros\n");
-    else {
-        pLivro livros = aloc(qtde);
-        preencher(livros, qtde);
-        mostrar(livros, qtde);
-        desaloc(livros);
+int main(){
+	
+	int qtde;
+	
+	scanf("%d", &qtde);
+	if(qtde <= 0)
+	printf("Sem livros\n");
+	
+	else{
+	pLivro livros = aloc(qtde);  
+	preencher(livros, qtde);
+	mostrar(livros, qtde);
+	desaloc(livros);
     }
-    
-    return 0;
+	
+	return 0;
 }
